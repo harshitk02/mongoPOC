a6ce8ecba149369de69b1bc493342e19b3dd8443
harshitk02 <106697735+harshitk02@users.noreply.github.com>
2024-04-28 01:03:23 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -160,4 +160,4 @@ def download_chart(filename):
         return jsonify({'error': 'File not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
